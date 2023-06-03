9ea5432141c159d60e7a67f0c7c38c6fce25e542
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2023-06-04 09:13:52 +1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function changetext(){
+    document.getElementById('h2').innerHTML = "hey i am change!"
+}
+
+// chage date and time
+function dateAndTime(){
+    document.getElementById('h4').innerHTML =Date();
+}
+// change date and time
+function dateAndTimee(){
+    document.getElementById('h5').innerHTML =Date();
+}
